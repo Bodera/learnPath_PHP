85daf0e1bb18b3e90ea26ec1d148a6e0bef93954
Bodera <rafaelrafa990@gmail.com>
2019-02-13 17:23:47 -0200

    Up

--- w3resources/108.returnURLcomponents.php
+++ w3resources/108.returnURLcomponents.php
@@ -1,5 +1,22 @@
 <?php
+#protocol://hostname/other_information
+#$uri = $_SERVER['REQUEST_URL'];
+$local_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+echo $local_url;
+parse_url($local_url);
+echo "<br/>";
+?>
 
+<?
+$url = $_SERVER;
 
+echo "<br/>";
+echo "Scheme : " . $url['SERVER_PROTOCOL']."<br/>";
+echo "Host : " . $url['SERVER_NAME']."<br/>";
+#echo "<br/>";
+echo "Port : " . $url['SERVER_PORT']."<br/>";
+echo "Path : " . $url['SCRIPT_NAME']."<br/>";
 
+$model_url = $url['SERVER_NAME'].':'.$url['SERVER_PORT'].$url['SCRIPT_NAME'];
+echo "Access link: " . $model_url;
 ?>
